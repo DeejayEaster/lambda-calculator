327eea5c39571c66a226b7e53859553348b425c2
deejayeaster <deejayeaster@live.com>
2019-07-17 20:17:51 -0400

    test, SSH

--- src/components/DisplayComponents/Logo.js
+++ src/components/DisplayComponents/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import logo from "../../Img/Lambda_Logo_white.png";
+import logo from "../../Img/new-logo.png";
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <img className="logo" src={logo} alt="Lambda's logo" />
+      <img className="logo" src={logo} alt="cool logo" />
     </div>
   );
 };
